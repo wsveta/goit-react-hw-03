aba12c3526e4d1b13c2472a02e16f9fd1465c434
Svitlana <wsv3ta@gmail.com>
2024-02-27 16:27:22 +0100

    update

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,7 +2,7 @@ import Contact from "./Contact";
 import css from "./ContactList.module.css";
 
 const ContactList = ({ contacts, onDelete}) => {
-    return (
+    return contacts !== '' && (
         <ul className={css.contacts}>
       {contacts.map((contact) => (
         <li className={css.contact} key={contact.id}>
